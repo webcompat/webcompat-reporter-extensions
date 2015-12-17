4f21e701f7df75c57dceb2c6c0eb947b983e5cda
Mike Taylor <miket@mozilla.com>
2015-12-17 16:42:39 -0600

    Fixes #15. Update Opera add-on to support screenshots.

--- opera/background.js
+++ opera/background.js
@@ -2,10 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var prefix = "http://webcompat.com/?open=1&url="
+var prefix = 'https://webcompat.com/?open=1&url=';
 
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-    chrome.tabs.create({ "url": prefix + encodeURIComponent(tab[0].url)});
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.captureVisibleTab({format: 'png'}, function(res) {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+      chrome.tabs.create({ "url": prefix + encodeURIComponent(tab[0].url)}, function(tab) {
+        chrome.tabs.executeScript({
+          code: 'window.postMessage("' + res + '", "*")'
+        });
+      });
+    });
   });
 });
