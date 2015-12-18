72f78fa9a360019cec698a122213d3870b9f9121
Mike Taylor <miket@mozilla.com>
2015-12-17 19:27:37 -0600

    Issue #13. Update Desktop SDK add-on to support screenshots.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,15 +2,60 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+let buttons = require("sdk/ui/button/action");
+let pageMod = require('sdk/page-mod')
 let tabs = require("sdk/tabs");
-let buttons = require('sdk/ui/button/action');
-let prefix = "http://webcompat.com/?open=1&url=";
+let tabUtils = require("sdk/tabs/utils");
+let { viewFor } = require("sdk/view/core");
+
+const DOMAIN = "https://webcompat.com/";
+const GET_ARGS = "?open=1&url=";
 
 function sendURL() {
-  var url = tabs.activeTab.url;
+  var tab = tabs.activeTab;
+  var url = tab.url;
+  var screenshotData;
+
   if (isReportableUrl(url)) {
-    tabs.open(prefix + encodeURIComponent(url));
+    screenshotData = getTabScreenshot(tab);
+    tabs.open({
+      url: DOMAIN + GET_ARGS + encodeURIComponent(url),
+      onOpen: function onOpen(tab) {
+        pageMod.PageMod({
+          include: [DOMAIN + '*'],
+          contentScript: 'window.postMessage("' + screenshotData + '", "*")',
+        });
+      }
+    });
   }
+}
+
+function getTabScreenshot(tab) {
+  // window that was active when the button was pressed.
+  var win = tabUtils.getBrowserForTab(viewFor(tab)).contentWindow;
+  var dpr = win.devicePixelRatio;
+  var canvas = win.document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var x = win.document.documentElement.scrollLeft;
+  var y = win.document.documentElement.scrollTop;
+  var w = win.innerWidth;
+  var h = win.innerHeight;
+  var scaled_w;
+  var scaled_h;
+
+  function setScale(scale) {
+    scaled_w = scale * w;
+    scaled_h = scale * h;
+    canvas.width = scaled_w;
+    canvas.height = scaled_h;
+    ctx.scale(scale, scale);
+  }
+
+  // TODO: if scale is bigger or equal to 2, try 2.
+  setScale(dpr);
+  ctx.drawWindow(win, x, y, w, h, '#ffffff');
+
+  return canvas.toDataURL();
 }
 
 function isReportableUrl(url) {
