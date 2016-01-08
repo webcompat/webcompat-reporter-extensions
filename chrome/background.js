43f618bded347c3eff40a83e3567d27c21f0b722
Mike Taylor <miket@mozilla.com>
2016-01-07 19:42:38 -0600

    Small tweak for chrome.

--- chrome/background.js
+++ chrome/background.js
@@ -3,13 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var prefix = 'https://webcompat.com/?open=1&url=';
+var screenshotData = '';
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.captureVisibleTab({format: 'png'}, function(res) {
+    screenshotData = res;
     chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
       chrome.tabs.create({ "url": prefix + encodeURIComponent(tab[0].url)}, function(tab) {
         chrome.tabs.executeScript({
-          code: 'window.postMessage("' + res + '", "*")'
+          code: 'window.postMessage("' + screenshotData + '", "*")'
         });
       });
     });
