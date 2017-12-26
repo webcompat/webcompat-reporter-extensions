2c26bac01a4a8b05089663713d727b4cd275a791
Mike Taylor <miket@mozilla.com>
2017-12-25 20:43:28 -0600

    Issue #61. Make eslint happy.

--- firefox/background.js
+++ firefox/background.js
@@ -2,23 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var prefix = 'https://webcompat.com/issues/new?url=';
-var screenshotData = '';
-var reporterID = 'addon-reporter-firefox';
+var prefix = "https://webcompat.com/issues/new?url=";
+var screenshotData = "";
+var reporterID = "addon-reporter-firefox";
 
 chrome.contextMenus.create({
-  id: 'webcompat-contextmenu',
-  title: 'Report site issue',
-  contexts: ['all']
+  id: "webcompat-contextmenu",
+  title: "Report site issue",
+  contexts: ["all"]
 });
 
 function reportIssue(tab) {
-  chrome.tabs.captureVisibleTab({format: 'png'}, function(res) {
+  chrome.tabs.captureVisibleTab({format: "png"}, function(res) {
     screenshotData = res;
     chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
       var newTabUrl =
         `${prefix}${encodeURIComponent(tab[0].url)}&src=${reporterID}`;
-      chrome.tabs.create({ 'url': newTabUrl}, function(tab) {
+      chrome.tabs.create({ "url": newTabUrl}, function(tab) {
         chrome.tabs.executeScript({
           code: `window.postMessage("${screenshotData}", "*")`
         });
@@ -37,9 +37,9 @@ function enableOrDisable(tabId, changeInfo, tab) {
     return ["http:", "https:"].includes(protocol);
   }
 
-  if (changeInfo.status == "loading" && isReportableURL(tab.url)) {
+  if (changeInfo.status === "loading" && isReportableURL(tab.url)) {
     chrome.browserAction.enable(tabId);
-  } else if (changeInfo.status == "loading" && !isReportableURL(tab.url)) {
+  } else if (changeInfo.status === "loading" && !isReportableURL(tab.url)) {
     chrome.browserAction.disable(tabId);
   }
 }
@@ -48,7 +48,7 @@ chrome.tabs.onCreated.addListener((tab) => {
   // disable all new tabs until they've loaded and we know
   // they have reportable URLs
   chrome.browserAction.disable(tab.tabId);
-})
+});
 chrome.tabs.onUpdated.addListener(enableOrDisable);
 chrome.contextMenus.onClicked.addListener(reportIssue);
 chrome.browserAction.onClicked.addListener(reportIssue);
