78641271764386320740ca59511a38f2401ed9bf
Mike Taylor <miket@mozilla.com>
2018-02-09 14:56:48 -0600

    No issue. Prettier/eslint fixes for Safari addon.js

--- safari/webcompat-reporter.safariextension/sendScreenshot.js
+++ safari/webcompat-reporter.safariextension/sendScreenshot.js
@@ -1,9 +1,19 @@
-safari.self.addEventListener("message", function(event){
-  if (event.name === "screenshot-data") {
-    window.postMessage(event.message, "*");
-  }
-}, false);
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-if (window === window.top && window.location.host == "webcompat.com") {
+/*global safari*/
+
+safari.self.addEventListener(
+  "message",
+  function(event) {
+    if (event.name === "screenshot-data") {
+      window.postMessage(event.message, "*");
+    }
+  },
+  false
+);
+
+if (window === window.top && window.location.host === "webcompat.com") {
   safari.self.tab.dispatchMessage("request-screenshot", "");
 }
