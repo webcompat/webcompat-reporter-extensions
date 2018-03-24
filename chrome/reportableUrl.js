626575ec898991d30b294d9dbb396372ac0efb59
OrionStar25 <chunnushrivastava@gmail.com>
2018-03-25 01:16:24 +0530

    fix bug

--- chrome/reportableUrl.js
+++ chrome/reportableUrl.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import initReportableUrl from "../shared/base.js";
+import isReportableURL from "../shared/base.js";
 
 const url = "//something";
 
