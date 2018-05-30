b7d38a27f7bc58da7f055b8063fe63455ca78aca
laghee <katrua@gmail.com>
2018-05-30 20:10:26 +0200

    Issue #126. Fix eslint/prettier conflict over switch cases.

--- tests/lib/helpers.js
+++ tests/lib/helpers.js
@@ -37,27 +37,27 @@ function compileWebpack() {
                   // Derive addon versioning from package.json
                   manifest["version"] = version;
                   switch (platform) {
-                  case "firefox":
-                    // Add Firefox-specific bits to the manifest.json
-                    manifest["applications"] = {
-                      gecko: {
-                        id: "jid1-mjpB54bRzP9Zxw@jetpack",
-                        strict_min_version: "49.0a1"
-                      }
-                    };
-                    return JSON.stringify(manifest, null, 2);
-                  case "firefox-mobile":
-                    // Add Firefox-mobile-specific bits to the manifest.json
-                    manifest["applications"] = {
-                      gecko: {
-                        id: "webcompat-reporter-for-mobile@webcompat.com",
-                        strict_min_version: "51.0"
-                      }
-                    };
-                    return JSON.stringify(manifest, null, 2);
-                  default:
-                    // Neither Chrome nor Opera needs the applications key
-                    return JSON.stringify(manifest, null, 2);
+                    case "firefox":
+                      // Add Firefox-specific bits to the manifest.json
+                      manifest["applications"] = {
+                        gecko: {
+                          id: "jid1-mjpB54bRzP9Zxw@jetpack",
+                          strict_min_version: "49.0a1"
+                        }
+                      };
+                      return JSON.stringify(manifest, null, 2);
+                    case "firefox-mobile":
+                      // Add Firefox-mobile-specific bits to the manifest.json
+                      manifest["applications"] = {
+                        gecko: {
+                          id: "webcompat-reporter-for-mobile@webcompat.com",
+                          strict_min_version: "51.0"
+                        }
+                      };
+                      return JSON.stringify(manifest, null, 2);
+                    default:
+                      // Neither Chrome nor Opera needs the applications key
+                      return JSON.stringify(manifest, null, 2);
                   }
                 }
               }
