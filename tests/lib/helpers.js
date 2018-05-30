15b363fc4b3e9ffa542c8a2d1caba77bd00d9a34
Kate Manning <kmanning@mills.edu>
2018-05-30 14:32:04 +0000

    Issue #126. Make helper.js use config template instead of multiple configs.

--- tests/lib/helpers.js
+++ tests/lib/helpers.js
@@ -2,20 +2,72 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 const webpack = require("webpack");
-// import webpack from "webpack";
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const version = require("../../package.json").version;
 
 /*
   Returns a promise that resolves once webpack has compiled all the addon
   configs have been built.
 */
-// export default function
 
 function compileWebpack() {
   return new Promise((resolve, reject) => {
-    const configs = ["chrome", "firefox", "firefox-mobile", "opera"].map(item =>
-      require(`../../${item}/webpack.config.js`)
+    const config = ["chrome", "firefox", "firefox-mobile", "opera"].map(
+      platform => {
+        var template = {
+          entry: `./${platform}/addon.js`,
+          output: {
+            filename: `../dist/${platform}/background.js`
+          },
+          plugins: [
+            new CopyWebpackPlugin([
+              {
+                from: "shared/content.js",
+                to: `../dist/${platform}/content.js`
+              },
+              {
+                from: "shared/*.png",
+                to: `../dist/${platform}/[name].[ext]`
+              },
+              {
+                from: "shared/manifest.json",
+                to: `../dist/${platform}/[name].json`,
+                transform: function(content, path) {
+                  let manifest = JSON.parse(content);
+                  // Derive addon versioning from package.json
+                  manifest["version"] = version;
+                  switch (platform) {
+                  case "firefox":
+                    // Add Firefox-specific bits to the manifest.json
+                    manifest["applications"] = {
+                      gecko: {
+                        id: "jid1-mjpB54bRzP9Zxw@jetpack",
+                        strict_min_version: "49.0a1"
+                      }
+                    };
+                    return JSON.stringify(manifest, null, 2);
+                  case "firefox-mobile":
+                    // Add Firefox-mobile-specific bits to the manifest.json
+                    manifest["applications"] = {
+                      gecko: {
+                        id: "webcompat-reporter-for-mobile@webcompat.com",
+                        strict_min_version: "51.0"
+                      }
+                    };
+                    return JSON.stringify(manifest, null, 2);
+                  default:
+                    // Neither Chrome nor Opera needs the applications key
+                    return JSON.stringify(manifest, null, 2);
+                  }
+                }
+              }
+            ])
+          ]
+        };
+        return template;
+      }
     );
-    const compiler = webpack(configs);
+    const compiler = webpack(config);
     compiler.run((err, stats) => {
       if (err) {
         reject(err);
