50391615c3945f80d759cf319eb9f3d5c2b897a1
laghee <katrua@gmail.com>
2018-07-14 20:06:05 +0200

    Issue #131. Update classname to hide extension download link on webcompat.com.

--- shared/content.js
+++ shared/content.js
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 // add a class to hide the download link when the add-on is installed.
-document.querySelector(".js-Navbar-link").classList.add("is-hidden");
+
+document.querySelector(".js-addon-link").classList.add("is-hidden");
